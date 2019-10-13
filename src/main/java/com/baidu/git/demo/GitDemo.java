ae6a6049e6690bc218d9249c114bc29e5555e08e
aden <aden.wang@qq.com>
2019-10-14 04:38:10 +0800

    添加1条记录

--- src/main/java/com/baidu/git/demo/GitDemo.java
+++ src/main/java/com/baidu/git/demo/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("GitDemo项目创建");
         System.out.println("GitDemo项目创建2");
+        System.out.println("添加1条记录");
     }
 }
