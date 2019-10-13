98fd0eb41764cbebdc19aef67d98c9b39f1610bc
aden <aden.wang@qq.com>
2019-10-14 04:46:08 +0800

    分支bbs创建，开始开发论坛

---
+++ src/main/java/com/baidu/git/demo/BranchDemo.java
@@ -0,0 +1,10 @@
+package com.baidu.git.demo;
+
+/**
+ * Created by Aden on 2019/10/14 4:41
+ */
+public class BranchDemo {
+    public static void main(String[] args) {
+        System.out.println("分支bbs创建，开始开发论坛");
+    }
+}
