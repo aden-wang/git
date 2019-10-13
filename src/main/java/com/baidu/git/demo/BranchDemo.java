43af136fa41480ac415595c701122deecac0a919
aden <aden.wang@qq.com>
2019-10-14 04:53:26 +0800

    分支bbs开发完成

--- src/main/java/com/baidu/git/demo/BranchDemo.java
+++ src/main/java/com/baidu/git/demo/BranchDemo.java
@@ -7,5 +7,6 @@ public class BranchDemo {
     public static void main(String[] args) {
         System.out.println("分支bbs创建，开始开发论坛");
         System.out.println("分支bbs开发第1天");
+        System.out.println("分支bbs开发完成");
     }
 }
